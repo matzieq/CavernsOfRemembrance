d789a92b180d60edfb971afd926b6cff31f77dc0
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2023-07-07 14:00:56 +0200

    Add movable stuff

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ struct Position {
     x: i32,
     y: i32,
 }
+#[derive(Component)]
+struct LeftMover {}
 
 #[derive(Component)]
 struct Renderable {
@@ -20,18 +22,40 @@ struct Renderable {
     bg: RGB,
 }
 
+
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
+
+        self.run_systems();
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
         for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph); 
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
     }
-
 }
 
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for (_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 {
+                pos.x = 70;
+            }
+        }
+    }
+}
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker {};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
@@ -42,6 +66,7 @@ fn main() -> rltk::BError {
 
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
@@ -61,6 +86,7 @@ fn main() -> rltk::BError {
                 fg: RGB::named(rltk::RED),
                 bg: RGB::named(rltk::BLACK),
             })
+            .with(LeftMover {})
             .build();
     }
 
