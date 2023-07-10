1381d4ee152d140031a500cb1d0c4a36cd0e1776
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2023-07-10 21:43:50 +0200

    Remove left movers

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,6 @@ struct Position {
     x: i32,
     y: i32,
 }
-#[derive(Component)]
-struct LeftMover {}
 
 #[derive(Component)]
 struct Renderable {
@@ -38,30 +36,15 @@ impl GameState for State {
     }
 }
 
-struct LeftWalker {}
-
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 {
-                pos.x = 70;
-            }
-        }
-    }
-}
 impl State {
     fn run_systems(&mut self) {
-        let mut lw = LeftWalker {};
-        lw.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
-    
+
     for (_player, pos) in (&mut players, &mut positions).join() {
         pos.x = min(79, max(0, pos.x + delta_x));
         pos.y = min(49, max(0, pos.y + delta_y));
@@ -70,14 +53,14 @@ fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
 
 fn player_input(gs: &mut State, ctx: &mut Rltk) {
     match ctx.key {
-        None => {},
+        None => {}
         Some(key) => match key {
-                VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-                VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-                VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-                VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-                _ => {}
-        }
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
     }
 }
 
@@ -89,7 +72,6 @@ fn main() -> rltk::BError {
     let mut gs = State { ecs: World::new() };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
 
     gs.ecs
@@ -102,19 +84,6 @@ fn main() -> rltk::BError {
         })
         .with(Player {})
         .build();
-
-    for i in 0..10 {
-        gs.ecs
-            .create_entity()
-            .with(Position { x: i * 7, y: 20 })
-            .with(Renderable {
-                glyph: rltk::to_cp437('%'),
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(LeftMover {})
-            .build();
-    }
 
     rltk::main_loop(context, gs)
 }
