2ddced353105fe0358508ffef3f14f87428acf2a
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2023-07-10 10:03:26 +0200

    Move player with cursor keys

--- src/main.rs
+++ src/main.rs
@@ -22,11 +22,13 @@ struct Renderable {
     bg: RGB,
 }
 
+#[derive(Component)]
+struct Player {}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-
+        player_input(self, ctx);
         self.run_systems();
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -56,17 +58,40 @@ impl State {
         self.ecs.maintain();
     }
 }
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+    
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {},
+        Some(key) => match key {
+                VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+                VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+                VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+                VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+                _ => {}
+        }
+    }
+}
+
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
         .with_title("Caverns of Remembrance")
         .build()?;
-
     let mut gs = State { ecs: World::new() };
-
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
+
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
@@ -75,6 +100,7 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(Player {})
         .build();
 
     for i in 0..10 {
