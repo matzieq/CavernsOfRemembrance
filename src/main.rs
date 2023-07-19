cd34d003ad9e31e1f649ab65fc6c38be8f6aadf0
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2023-07-19 09:41:43 +0200

    Add basic monster code

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ pub use rect::*;
 mod visibility_system;
 pub use visibility_system::*;
 
-
 pub struct State {
     ecs: World,
 }
@@ -22,17 +21,21 @@ impl GameState for State {
         ctx.cls();
         player_input(self, ctx);
         self.run_systems();
+        draw_map(&self.ecs, ctx);
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
-        draw_map(&self.ecs, ctx);
+        let map = self.ecs.fetch::<Map>();
         for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            let idx = map.xy_idx(pos.x, pos.y);
+            if map.visible_tiles[idx] {
+                ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            }
         }
     }
 }
 impl State {
     fn run_systems(&mut self) {
-        let mut vis = VisibilitySystem{};
+        let mut vis = VisibilitySystem {};
         vis.run_now(&self.ecs);
         self.ecs.maintain();
     }
@@ -49,11 +52,13 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Viewshed>();
     let map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
-    gs.ecs.insert(map);
 
     gs.ecs
         .create_entity()
-        .with(Position { x: player_x, y: player_y })
+        .with(Position {
+            x: player_x,
+            y: player_y,
+        })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
@@ -67,5 +72,24 @@ fn main() -> rltk::BError {
         })
         .build();
 
+    for room in map.rooms.iter().skip(1) {
+        let (x, y) = room.center();
+        gs.ecs
+            .create_entity()
+            .with(Position { x, y })
+            .with(Renderable {
+                glyph: rltk::to_cp437('g'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .with(Viewshed {
+                visible_tiles: Vec::new(),
+                range: 8,
+                dirty: true,
+            })
+            .build();
+    }
+
+    gs.ecs.insert(map);
     rltk::main_loop(context, gs)
 }
